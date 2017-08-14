66ce93014f5722398c751404dc0130c3d185576f
fuzhong <oicelancer@gmail.com>
2017-08-14 11:15:56 +0800

    create project.

---
+++ SimpleSwift/SimpleSwift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SimpleSwift
+//
+//  Created by fuzhong on 2017/8/14.
+//  Copyright © 2017年 fuzhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("test")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
